820dc4c0956af843a587fdb6b6599b2d3ea4eea1
Yull Bazurto <ybazurto@uees.edu.ec>
2024-09-05 08:01:18 -0500

    Incio de clase

---
+++ ejer1.py
@@ -0,0 +1,12 @@
+a=15
+b=14
+c=13
+d=12
+e=11
+r=13
+h=15
+g=3
+u=3
+z=32
+suma=a+b+c+d+e+r+h+g+u+z
+print("El resultado de la suma es ",suma)
---
+++ interpretado.py
@@ -0,0 +1,4 @@
+a=10
+b=35
+s=a+b
+print("Suma es igual",s)
